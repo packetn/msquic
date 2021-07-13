e5056ffe3056e6628080f61ae96e02e6608de143
Ernest Wilkerson <ernest.wilkerson@gmail.com>
2021-07-13 02:29:08 -0700

    Debug build to figure out issues with container

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ git submodule update --init --recursive
 if [ -d "build" ]; then rm -Rf build; fi
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -j 8 && make install
